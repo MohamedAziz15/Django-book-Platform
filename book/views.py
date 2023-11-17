d48e5c83fa3c59113dd9eb13ec26dddefdd81a76
MohamedAziz15 <mohamedazizmahmoud@gmail.com>
2023-11-17 13:33:29 +0200

    add templates & make templates  inherit from base.html

--- book/views.py
+++ book/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Book,Author,review
 
 # Create your views here.
+
+def book_list(request):
+    books = Book.objects.all()
+    return render(request,'book/book_list.html',{'books':books})
+
